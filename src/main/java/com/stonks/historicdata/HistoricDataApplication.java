2661675b3d9b4e8fb2169f5ee939067e55728b47
vinny <vinnytorde@gmail.com>
2021-05-22 17:17:59 -0500

    init

---
+++ src/main/java/com/stonks/historicdata/HistoricDataApplication.java
@@ -0,0 +1,13 @@
+package com.stonks.historicdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stonks/historicdata/HistoricDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stonks.historicdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
